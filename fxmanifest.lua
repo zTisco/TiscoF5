6bd2bc83bbaab6a5254229e5135fefb63046554e
ItsZmainDev <zmainpro@gmail.com>
2024-07-09 22:38:55 +0200

    Micro update of source code

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,25 +1,26 @@
-fx_version 'adamant'
-game 'gta5'
-author 'Tisco'
+fx_version "bodacious"
+game "gta5"
+author "Tisco"
+description "Menu F5 pour les joueurs de votre serveur FiveM (ESX) - Tisco Menu F5 (Discord : ztisco)"
 
 client_scripts {
-    'src/RMenu.lua',
-    'src/menu/RageUI.lua',
-    'src/menu/Menu.lua',
-    'src/menu/MenuController.lua',
-    'src/components/*.lua',
-    'src/menu/elements/*.lua',
-    'src/menu/items/*.lua',
-    'src/menu/panels/*.lua',
-    'src/menu/panels/*.lua',
-    'src/menu/windows/*.lua',
+    "src/RMenu.lua",
+    "src/menu/RageUI.lua",
+    "src/menu/Menu.lua",
+    "src/menu/MenuController.lua",
+    "src/components/*.lua",
+    "src/menu/elements/*.lua",
+    "src/menu/items/*.lua",
+    "src/menu/panels/*.lua",
+    "src/menu/panels/*.lua",
+    "src/menu/windows/*.lua",
 
-    'client.lua',
+    "client.lua",
 }
 
 server_scripts {
-    '@mysql-async/lib/MySQL.lua',
-    'server.lua',
+    "@mysql-async/lib/MySQL.lua",
+    "server.lua",
 }
 
-shared_script 'config.lua'
+shared_script "config.lua"
